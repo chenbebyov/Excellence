92b4a4c9f62b94deb0cc9b91323a183074c718d9
chenbebyov <chenbebyov2000@gmail.com>
2021-01-17 22:03:36 +0200

    adding a hierarchy

--- server/models/group-model.js
+++ server/models/group-model.js
@@ -2,32 +2,48 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Group = new Schema(
-    {
-        groupName: { type: String, required: true },
-        teacherCode: { type: int, required: true },
-        LevelOfLearning: { type: int, enum:['level a','level b','level c'],required: true },
-        timeLesson: {
-            type:[{
-                date: { type: date, required: true },
-                fromAnHour: { type: date, required: true },
-                toAnHour: { type: date, required: true },
+    {  
+     layers: {
+        type:[{
+            name:{ type: String, required: true },
+            class: {
+                type:[{
+                    name:{ type: String, required: true },
+                    levels: {
+                        type:[{
+                            LevelOfLearning: { type: int, enum:['level a','level b','level c'],required: true },
+                            teacherCode: { type: int, required: true },
+                            timeLesson: {
+                                type:[{
+                                    date: { type: date, required: true },
+                                    fromAnHour: { type: date, required: true },
+                                    toAnHour: { type: date, required: true },
+                                }],
+                                required: true
+                           },
+                            lesson: {
+                                type:[{
+                                    Code: { type: int, required: true },
+                                    remarks: { type: String, required: false },
+                                    date: { type: date, required: true }
+                                }],
+                                required: true
+                           }, 
+                            StudentsInTheGroup: {
+                                type:[{
+                                    studentCode: { type: int, required: true },
+                                }],
+                                required: true
+                           },        
+                        }],
+                        required: true
+                    },
+                  }],
+                required: true
+               },
             }],
-            required: true
+        required: true
        },
-        lesson: {
-            type:[{
-                Code: { type: int, required: true },
-                remarks: { type: String, required: false },
-                date: { type: date, required: true }
-            }],
-            required: true
-       }, 
-        StudentsInTheGroup: {
-            type:[{
-                studentCode: { type: int, required: true },
-            }],
-            required: true
-       },    
     },    
     { timestamps: true },
 )
