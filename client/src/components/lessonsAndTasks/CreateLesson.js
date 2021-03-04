bb0dcd1d33e41f3af1c81ae6004bf582455fc07e
Nechama Kremer <nechama.kremer@credorax.com>
2021-03-04 09:10:41 +0200

    updates

--- client/src/components/lessonsAndTasks/CreateLesson.js
+++ client/src/components/lessonsAndTasks/CreateLesson.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Form, Input, Button,Upload, message  } from 'antd';
+import { Form, Input, Button, Upload, message, Row, Col } from 'antd';
+
 import { useDispatch } from 'react-redux';
 import { createLesson } from '../../redux/actions/lesson.actions';
-import { InboxOutlined ,UploadOutlined} from '@ant-design/icons';
-import '../css/Lessons.css';
+import { InboxOutlined, UploadOutlined } from '@ant-design/icons';
+import '../../css/Lessons.css';
 
 
 const layout = {
     labelCol: {
-      span: 8,
+        span: 8,
     },
     wrapperCol: {
-      span: 16,
+        span: 16,
     },
 };
 
@@ -21,130 +22,126 @@ const CreateLesson = () => {
 
     const { Dragger } = Upload;
 
-const props = {
-  name: 'file',
-  multiple: true,
-  action: '                                                ',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-};
+    const props = {
+        name: 'file',
+        multiple: true,
+        action: '                                                ',
+        onChange(info) {
+            const { status } = info.file;
+            if (status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (status === 'done') {
+                message.success(`${info.file.name} file uploaded successfully.`);
+            } else if (status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+    };
 
     const save = (values) => {
-        console.log('Success:', values); 
-        dispatch(createLesson(values));       
+        console.log('Success:', values);
+        dispatch(createLesson(values));
     }
-    
+
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
 
     return (
-      <>
-     <div className="parent">
-        <Form {...layout} name="Add New Lesson" initialValues={{ remember: true }} onFinish={save} onFinishFailed={onFinishFailed} >
-        <div className="div1">
-        <Form.Item
-            label="lesson subject"
-            name="lessonSubject"
-            rules={[
-                {
-                    required: true,
-                    pattern:"(^[a-zA-Z0-9]+$)",
-                    min:2,
-                    message: 'lesson subject is required and must be at least 2 chars',
-                },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-      </div>
-      <div className="div2">
-        <Form.Item
-            label="file name"
-            name="fileName"
-            rules={[
-                {
-                    required: true,
-                    pattern:"(^[a-zA-Z0-9]+$)",
-                    min:2,
-                    message: 'file name is required and must be at least 2 chars',
-                },
-            ]}
-        >
-            <Input />
+        <>
+        <Row>
+            <Col span={12} offset={6}>
+                <Form className="parent" 
+                        {...layout} 
+                        name="Add New Lesson" 
+                        initialValues={{ remember: true }} 
+                        onFinish={save} 
+                        onFinishFailed={onFinishFailed}>
 
-        </Form.Item>
-        </div>
-        <div className="div3">
-        <Form.Item>
-        <Dragger {...props}>
-          <p className="ant-upload-drag-icon">
-            <InboxOutlined />
-         </p>
-         <p className="ant-upload-text">Click or drag file to this area to upload</p>
-         <p className="ant-upload-hint">
-            Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-            band files
-         </p>
-       </Dragger>
-       <Upload action="                                                " directory>
-           <Button icon={<UploadOutlined />}>Upload Directory</Button>
-       </Upload>
-     </Form.Item>
-      </div>
-      <div className="div4">
-       <Form.Item
-            label="task name"
-            name="taskName"
-            rules={[
-                {
-                    required: true,
-                    pattern:"(^[a-zA-Z0-9]+$)",
-                    min:2,
-                    message: 'file name is required and must be at least 2 chars',
-                },
-            ]}
-        >
-            <Input />
-        </Form.Item>
-      </div>
-      <div className="div5">
-<Form.Item>
-      <Dragger {...props}>
-        <p className="ant-upload-drag-icon">
-         <InboxOutlined />
-       </p>
-      <p className="ant-upload-text">Click or drag file to this area to upload</p>
-      <p className="ant-upload-hint">
-         Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-         band files
-      </p>
-     </Dragger>
-     <Upload action="                                                " directory>
-           <Button icon={<UploadOutlined />}>Upload Directory</Button>
-       </Upload>
-</Form.Item>
-</div>
-<div className="div6">
-        <Form.Item>
-            <Button type="primary" htmlType="submit">
-                Add Lesson
-            </Button>
-        </Form.Item>
-</div>
-       
-    </Form>
-    </div>
-    </>
+                        <Form.Item className="div1"
+                            label="lesson subject"
+                            name="lessonSubject"
+                            rules={[
+                                {
+                                    required: true,
+                                    pattern: "(^[a-zA-Z0-9]+$)",
+                                    min: 2,
+                                    message: 'lesson subject is required and must be at least 2 chars',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item className="div2"
+                            label="file name"
+                            name="fileName"
+                            rules={[
+                                {
+                                    required: true,
+                                    pattern: "(^[a-zA-Z0-9]+$)",
+                                    min: 2,
+                                    message: 'file name is required and must be at least 2 chars',
+                                },
+                            ]}
+                        >
+                            <Input />
+
+                        </Form.Item>
+                        <Form.Item className="div3">
+                            <Dragger {...props}>
+                                <p className="ant-upload-drag-icon">
+                                    <InboxOutlined />
+                                </p>
+                                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                <p className="ant-upload-hint">
+                                    Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                                    band files
+                                </p>
+                            </Dragger>
+                            <Upload action="                                                " directory>
+                                <Button icon={<UploadOutlined />}>Upload Directory</Button>
+                            </Upload>
+                        </Form.Item>
+                        <Form.Item
+                            className="div4"
+                            label="task name"
+                            name="taskName"
+                            rules={[
+                                {
+                                    required: true,
+                                    pattern: "(^[a-zA-Z0-9]+$)",
+                                    min: 2,
+                                    message: 'file name is required and must be at least 2 chars',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item className="div5">
+                            <Dragger {...props}>
+                                <p className="ant-upload-drag-icon">
+                                    <InboxOutlined />
+                                </p>
+                                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                <p className="ant-upload-hint">
+                                    Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                                    band files
+                                </p>
+                            </Dragger>
+                            <Upload action="                                                " directory>
+                                <Button icon={<UploadOutlined />}>Upload Directory</Button>
+                            </Upload>
+                        </Form.Item>
+                        <Form.Item className="div6">
+                            <Button type="primary" htmlType="submit">
+                                Add Lesson
+                            </Button>
+                        </Form.Item>
+                </Form>
+            </Col>
+        </Row>
+        </>
     )
 
 }
--- server/controllers/group-ctrl.js
+++ server/controllers/group-ctrl.js
@@ -1,4 +1,6 @@
 const Layer = require('../models/layer-model');
+const Group = require('../models/group-model');
+const ObjectId = require('mongoose/lib/types/objectid');
 
 
 updateGroup = (req, res) => {
@@ -10,27 +12,16 @@ updateGroup = (req, res) => {
             error: 'Failed to update group, details are empty.',
         })
     }
-    Layer.aggregate([
-        { $unwind: "$grades"},
-        { $unwind: "$grades.levels"},
-        { $unwind: "$grades.levels.groups"},
-        {
-            $match: {
-                "grades.levels.groups._id": body.groupId
-            }
-        }
-    ]).then(group => {
-        if (group) {
-            return res.status(200).json({
-                success: true,
-                data:group
-            })
-        }
-        
-    }).catch(err => res.status(400).json({ success: false, error: err }));
-
-
-    // Layer.findById({"grades.levels.groups._id":body.groupId},{"groups.$._id": 1}).then(group => {
+    // Layer.aggregate([
+    //     { $unwind: "$grades"},
+    //     { $unwind: "$grades.levels"},
+    //     { $unwind: "$grades.levels.groups"},
+    //     {
+    //         $match: {
+    //             "grades.levels.groups._id": body.groupId
+    //         }
+    //     }
+    // ]).then(group => {
     //     if (group) {
     //         return res.status(200).json({
     //             success: true,
@@ -39,7 +30,69 @@ updateGroup = (req, res) => {
     //     }
         
     // }).catch(err => res.status(400).json({ success: false, error: err }));
+
+
+    // Group.find({}).then(group => {
+    //     if (group) {
+    //         return res.status(200).json({
+    //             success: true,
+    //             data:group
+    //         })
+    //     }
+        
+    // }).catch(err => res.status(400).json({ success: false, error: err }));
+
+    // .findOne({
+    //     ...    "blog_id" : ObjectId("56587befdb7224110f007233")
+    //     ... },{
+    //     ...    "comments": { $elemMatch: { _id: ObjectId("565f1034fd07cbfc1129db0b") } }
+    //     ... })
     
+    // Layer.findOne({"grades.levels.groups._id" : body.groupId},"grades.levels.groups.$").then(group => {
+    //     if (group) {
+    //         return res.status(200).json({
+    //             success: true,
+    //             data:group
+    //         })
+    //     }
+
+    // }).catch(err => res.status(400).json({ success: false, error: err }));
+    // Layer.findOne({"grades.levels.groups._id" : body.groupId},"grades.levels.groups.$").then(group => {
+    //     if (group) {
+    //         return res.status(200).json({
+    //             success: true,
+    //             data:group
+    //         })
+    //     }
+
+    // }).catch(err => res.status(400).json({ success: false, error: err }));
+
+    // Layer.findOne({'grades.levels.groups.0._id': body.groupId}).then(group => {
+    //     if (group) {
+    //         return res.status(200).json({
+    //             success: true,
+    //             data:group
+    //         })
+    //     }
+
+    // }).catch(err => res.status(400).json({ success: false, error: err }));
+
+    Layer.aggregate()
+    .match({'grades.levels.groups._id': body.groupId})
+    .project({
+        'groups': {
+            '$filter': {
+                'input': '$groups',
+                'cond': {
+                    '$eq': ['$$groups._id', body.groupId]
+                }
+            }
+        }
+    })
+    .exec(function (err, res) {
+        if (err) return handleError(err);
+        console.log(res); // [ { orders: [...] } ]
+    });
 
     //TODO:...
 }
