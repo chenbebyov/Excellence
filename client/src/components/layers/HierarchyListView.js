73d3f17b7358ce9ea144b6a02a6dc2d2da173360
chenbebyov <chenbebyov2000@gmail.com>
2021-05-09 04:34:04 +0300

    uptade

--- client/src/components/layers/HierarchyListView.js
+++ client/src/components/layers/HierarchyListView.js
@@ -178,33 +178,3 @@ const HierarchyListView = (props) => {
 }
 
 export default HierarchyListView;
-
-// import React, { useState } from 'react';
-// import { Modal, Button } from 'antd';
-
-// const App = () => {
-//   const [isModalVisible, setIsModalVisible] = useState(false);
-
-//   const showModal = () => {
-//     setIsModalVisible(true);
-//   };
-
-//   const handleOk = () => {
-//     setIsModalVisible(false);
-//   };
-
-//   const handleCancel = () => {
-//     setIsModalVisible(false);
-//   };
-
-//   return (
-//     <>
-//       <Button type="primary" onClick={showModal}>
-//         Open Modal
-//       </Button>
-//       <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-
-//       </Modal>
-//     </>
-//   );
-// };
